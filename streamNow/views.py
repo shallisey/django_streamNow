ca62181336b0fe58fbd1660cd0c2a95a8f08dd51
shallisey <halliseyseanshsu@gmail.com>
2021-09-08 20:56:53 -0500

    Setup display view for single media_type. Need to add form for Posts to be made that are correlated to the movie the are posted about.

--- streamNow/views.py
+++ streamNow/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import View, DetailView
+from django.views.generic import View
+from django.views.generic.detail import DetailView
 from .models import Post
 import requests
 import pprint
@@ -51,10 +52,36 @@ class MediaDetailView(DetailView):
         # If there is a response then add values to the context
         # Title, oveview, image, rating, genres, maybe similar movies
         if response:
-            print(response)
+            context['media_type'] = media_type
+            context['_id'] = _id
 
+            # media_type is TV
+            if media_type == 'tv':
+                context['name'] = response['name']
+                context['num_of_episodes'] = response['number_of_episodes']
+                context['num_of_seasons'] = response['number_of_seasons']
+            # media_type is movie
+            else:
+                context['name'] = response['original_title']
+                context['imdb_id'] = response['imdb_id']
 
+            context['overview'] = response['overview']
+            context['poster_path'] = response['poster_path']
+            context['backdrop_path'] = response['backdrop_path']
+            context['popularity'] = response['popularity']
+            context['vote_average'] = response['vote_average']
+            context['genres'] = response['genres']
+            context['production_companies'] = response['production_companies']
+            context['release_date'] = response['release_date']
+            context['runtime'] = response['runtime']
+            context['revenue'] = '${:,.2f}'.format(response['revenue'])
 
+        if response['homepage']:
+            context['homepage'] = response['homepage']
+        if response['tagline']:
+            context['tagline'] = response['tagline']
+
+        print(context['production_companies'])
 
         return render(request, 'streamNow/media_detail.html', context)
 
